819e7f5534118f28b25e319b4203d86c051e8717
Marcela Bomfim <mbomfim@live.com>
2019-10-18 00:47:46 -0400

    Created POST /projects route

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const projects = [];
+
 app.get('/', (req, res) => {
   return res.send('GoStack Challenge 01!');
+});
+
+app.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  projects.push({
+    id,
+    title,
+    taks: []
+  });
+
+  return res.json({ projects });
 });
 
 app.listen(3000);
